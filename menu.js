027972963e5d08560a06f01b6d2627ab6bbf3a36
katamarang11 <arestov.maks@mail.ru>
2023-03-08 15:45:13 +0300

    commit

--- menu.js
+++ menu.js
@@ -3,6 +3,10 @@ const headerMenuBtnClose = document.querySelector('.header__menu-btn_close');
 const headerNavHidden = document.querySelector('.header__nav-hidden');
 const body = document.querySelector('.body');
 
+const headerOrderBtn = document.querySelector('.header__order-btn');
+const modalBtnClose = document.querySelector('.modal__btn-close');
+const modalWindow = document.querySelector('.modal-window');
+
 headerMenuBtnOpen.addEventListener('click', function () {
   headerNavHidden.classList.add('header__nav-hidden_none');
   headerMenuBtnOpen.classList.add('header__menu-btn_hide');
@@ -10,10 +14,18 @@ headerMenuBtnOpen.addEventListener('click', function () {
   body.classList.add('body-scroll-no');
 });
 
-
 headerMenuBtnClose.addEventListener('click', function () {
   headerNavHidden.classList.remove('header__nav-hidden_none');
   headerMenuBtnOpen.classList.remove('header__menu-btn_hide');
   headerMenuBtnClose.classList.add('header__menu-btn_hide');
   body.classList.remove('body-scroll-no');
+});
+
+
+headerOrderBtn.addEventListener('click', function () {
+  modalWindow.classList.remove('modal-window__hidden');
+});
+
+modalBtnClose.addEventListener('click', function () {
+  modalWindow.classList.add('modal-window__hidden');
 });
